a744826dff5ce731326c340f87456edc70a6f953
techbase2brand <techbase2brand@gmail.com>
2026-07-30 13:23:13 +0530

    07-30-26 dfhdfjdfjdgh

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
